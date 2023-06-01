6e9811ad32d22f0455c104dc182d5a7350091626
eriket0107 <ol.erik0107@gmail.com>
2023-06-01 20:36:41 -0300

    refact at checkout page

--- src/pages/Checkout/components/styles.ts
+++ src/pages/Checkout/components/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
+export const CheckoutMain = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 2.5rem;
+  width: 100%;
+  gap: 2rem;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`
+
 export const CheckoutAddressContainer = styled.form`
   display: flex;
   flex-direction: column;
